37b75b996669305973cb302911072f46da1c41e8
BBVG/BBVS <134229346+BlockBearsV1@users.noreply.github.com>
2023-09-16 03:03:28 -0400

    Update bbearsrenewableenergynft.sol

--- contracts/bbearsrenewableenergynft.sol
+++ contracts/bbearsrenewableenergynft.sol
@@ -1,14 +1,11 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract BBearsRenewableEnergyNFT is ERC721 {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
-    
+    uint256 private tokenIdCounter;
+
     struct RenewableEnergyNFT {
         string energyType;
         uint256 productionDate;
@@ -17,7 +14,7 @@ contract BBearsRenewableEnergyNFT is ERC721 {
         uint256 price;
         bool isListed;
     }
-    
+
     mapping(uint256 => RenewableEnergyNFT) public nfts;
 
     constructor() ERC721("BBears Renewable Energy NFT", "BBREN") {}
@@ -30,7 +27,7 @@ contract BBearsRenewableEnergyNFT is ERC721 {
         uint256 _price
     ) public returns (uint256) {
         require(bytes(_energyType).length > 0, "Energy type must be provided");
-        
+
         RenewableEnergyNFT memory newNft = RenewableEnergyNFT({
             energyType: _energyType,
             productionDate: _productionDate,
@@ -39,12 +36,13 @@ contract BBearsRenewableEnergyNFT is ERC721 {
             price: _price,
             isListed: false
         });
-        
-        _tokenIds.increment();
-        uint256 newTokenId = _tokenIds.current();
+
+        uint256 newTokenId = tokenIdCounter;
+        tokenIdCounter++;
+
         _mint(msg.sender, newTokenId);
         nfts[newTokenId] = newNft;
-        
+
         return newTokenId;
     }
 
@@ -66,18 +64,15 @@ contract BBearsRenewableEnergyNFT is ERC721 {
     function approve(address approved, uint256 tokenId) public override {
         address owner = ownerOf(tokenId);
         require(approved != owner, "Already approved");
-        
         require(
             msg.sender == owner || isApprovedForAll(owner, msg.sender),
             "Not authorized"
         );
-        
         _approve(approved, tokenId);
     }
-    
+
     function setApprovalForAll(address operator, bool approved) public override {
         require(operator != msg.sender, "Cannot self-approve");
-        
         setApprovalForAll(operator, approved);
     }
 }
